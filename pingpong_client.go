fca21b27da77b1222c08e383780fe0f86275bb7e
Your Name <you@example.com>
2021-09-19 10:29:00 +0000

    update

--- pingpong_client.go
+++ pingpong_client.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"time"
 
-	pb "github.com/livman/go-sample-module/pingpong"
+	pb "./pingpong"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
